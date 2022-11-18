0356a0935a33f4841d0e49a96a17f26ae08745ab
miaout11 <miaout.11@gmail.com>
2022-11-18 22:14:11 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,18 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// create todo
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+router.post('/', (req, res) => {
+  const UserId = req.user.id  // 增加限制條件，只有登入者本人可以增刪找查到自己的資料
+  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name, UserId }) // call Todo 直接新增資料
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
 // detail page
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
