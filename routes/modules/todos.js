0fdd2ea5654f4f597334392029d50b48282eb057
miaout11 <miaout.11@gmail.com>
2022-11-18 22:26:14 +0800

    add update todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-// create todo
+// new page
 router.get('/new', (req, res) => {
   return res.render('new')
 })
+// create todo
 router.post('/', (req, res) => {
   const UserId = req.user.id  // 增加限制條件，只有登入者本人可以增刪找查到自己的資料
   const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
@@ -25,6 +26,33 @@ router.get('/:id', (req, res) => {
     // 把資料轉換成 plain object 的方法，只需要直接在傳入樣板前加上 toJSON()
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
+})
+
+// edit page
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({
+    where: { id, UserId }
+  })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.error(error))
+})
+// update todo
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({
+    where: { id, UserId }
+  })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
