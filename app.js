07f003e91c32cb212c91abbdf404018837d870ee
miaout11 <miaout.11@gmail.com>
2022-11-18 18:56:00 +0800

    add index page

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ app.use(methodOverride('_method'))
 
 // setting routes
 app.get('/', (req, res) => {
-  res.send(`<h1>hello world<h1>`)
+  // 查詢多筆資料是 findAll()，如果不帶參數，會預設撈出整張表的資料
+  return Todo.findAll({
+    // 把資料轉換成單純 JS 物件，需要將這包參數{ raw: true, nest: true}傳給 findAll()，才能在樣板裡使用資料
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.get('/', (req, res) => {
